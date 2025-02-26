bf281403569b214ff6f1372b6ee7ba8b172046bf
sinndu <46204105+sinndu@users.noreply.github.com>
2025-02-25 23:54:24 -0500

    Finished HDF5 file making, and basic time vs acceleration plots

---
+++ python_files/step2_data_storage.py
@@ -0,0 +1,59 @@
+# Making HDF5 File
+import h5py
+import pandas as pd
+import numpy as np
+import os
+from glob import glob
+
+hdf5_path = "../outputs/raw_data.hdf5" # Output Path
+
+# Raw Data Folders
+data_folders = {
+    "sindu": "../raw_data/sindu",
+    "logan": "../raw_data/logan",
+    "finn": "../raw_data/finn",
+}
+
+# Required Data Columns, assigned to keywords
+required_columns = {
+    "Time (s)": "time",
+    "Acceleration x (m/s^2)": "x",
+    "Acceleration y (m/s^2)": "y",
+    "Acceleration z (m/s^2)": "z",
+    "Absolute acceleration (m/s^2)": "abs_acc"
+}
+
+with h5py.File(hdf5_path, "w") as hdf: # Opening/Creating HDF5 File
+    # Raw Data & Pre-Processed Data (Need User Groups!)
+    for user, folder in data_folders.items(): # Iterate over each user/folder
+        user_raw_group = hdf.create_group(f"raw/{user}")  # Create user group based of user
+        user_processed_group = hdf.create_group(f"processed/{user}")  # Create user group based of user
+
+        user_walking_data_group = hdf.create_group(f"raw/{user}/walking")  # Create another hdf group
+        user_jumping_data_group = hdf.create_group(f"raw/{user}/jumping")  # Create another hdf group
+
+
+        csv_files = glob(os.path.join(folder, "*.csv")) # Get all files in directory
+
+        for file in csv_files:
+            df = pd.read_csv(file) # Read CSV into DataFrame
+
+            if set(required_columns.keys()).issubset(df.columns): # Check to make sure all columns are there (should always be true due to how data is collected)
+                df = df.rename(columns = required_columns) # Rename columns to easier keywords
+                data_array = df[['time', 'x', 'y', 'z', 'abs_acc']].to_numpy() # Convert to numpy array
+
+                dataset_name = os.path.splitext(os.path.basename(file))[0] # Gets name of dataset without .csv (for naming)
+                dataset_type = dataset_name.split("_")[1] # Get dataset type (always second word, i.e sindu_walking_etc)
+
+                if dataset_type == "walking": # If walking
+                    user_walking_data_group.create_dataset(dataset_name, data=data_array)  # Store into HDF5
+                elif dataset_type == "jumping": # If jumping
+                    user_jumping_data_group.create_dataset(dataset_name, data=data_array)  # Store into HDF5
+
+
+
+    # Segmented Data
+    train_group = hdf.create_group("segmented/train") # Train Group
+    test_group = hdf.create_group("segmented/test") # Test Group
+
+
---
+++ python_files/step3_visualization_plots.py
@@ -0,0 +1,55 @@
+# Acceleration vs Time Plots for Dataset
+import matplotlib as mpl
+import h5py
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+#######################################################################################################################################################
+# If you're running this code to make a plot, all you SHOULD (if i didnt fuck it up) need to do is change this axis to whatever parameter you want.
+acceleration_axis = "y"  # Options: x, y, z, abs_acc
+#######################################################################################################################################################
+
+axis_map = {"x": 1, "y": 2, "z": 3, "abs_acc" : 4} # Map axis to the index they are in the HDF5 (it's stored as a numpy)
+axis_index = axis_map[acceleration_axis] # Get index
+
+hdf5_path  = "../outputs/raw_data.hdf5" # Get HDF5 file
+output = "../outputs/plots"
+
+data = {"walking": [], "jumping": []}  # Store data for walking & jumping
+
+# Load all the data
+with h5py.File(hdf5_path, "r") as hdf: # Read file
+    for user in hdf["raw"]: # Loop through all users
+        for activity in ["walking", "jumping"]: # Make sure group is either walking or jumping
+            activity_path = f"raw/{user}/{activity}"
+            if activity_path in hdf: # If it's in the HDF
+                for dataset_name in hdf[activity_path]:  # Loop through datasets
+                    dataset = hdf[f"{activity_path}/{dataset_name}"][:]  # Load dataset
+                    time = dataset[:, 0]  # Get time
+                    acceleration = dataset[:, axis_index]  # Get selected acceleration
+                    data[activity].append((time, acceleration))  # Store the data
+
+# Function for plotting data. Passes in LIST of activities, LIST of colors, and filename that it writes to. Must be LISTs (even individual plots need to be in list format)
+def plot_activity(activities, colors, filename):
+    plt.figure(figsize=(10, 5)) # Create 10x5" figure
+
+    for activity, color in zip(activities, colors): # Loop through each activity, associate it with the color list
+        for time, acceleration in data[activity]: # Plot data
+            plt.plot(time, acceleration, label=activity.capitalize(), color=color, alpha=0.5)
+
+    plt.xlabel("Time") # Set X Label
+    plt.ylabel(f"{acceleration_axis.upper()} Acceleration")  # Set Y Label (dependant on acceleration)
+
+    plt.title(f"{acceleration_axis.upper()} Acceleration for {', '.join([a.capitalize() for a in activities])} Across All Users") # Set title
+
+    plt.legend([a.capitalize() for a in activities]) # Set legend
+    plt.grid() # Add grid
+
+    plt.savefig(os.path.join(output, filename), dpi=300) # Save file
+    plt.close()
+
+plot_activity(["walking"], ["g"], f"{acceleration_axis}_acceleration_walking.png") # Walking
+plot_activity(["jumping"], ["b"], f"{acceleration_axis}_acceleration_jumping.png") # Jumping
+plot_activity(["walking", "jumping"], ["g", "b"], f"{acceleration_axis}_acceleration_walking_jumping.png") # Both
+
